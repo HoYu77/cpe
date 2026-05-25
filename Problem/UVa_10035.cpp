7d677de5071cb27c4613a91afc266f5f3326f3b6
HoYu77 <a1145530@mail.nuk.edu.tw>
2026-05-25 07:05:57 +0000

    finished

--- Problem/UVa_10035.cpp
+++ Problem/UVa_10035.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
     int a,b;
@@ -6,30 +6,25 @@ int main(){
         if(a==0&&b==0){
             break;
         }
+        int carry=0;
+        int cnt=0;
+        while(a||b){
+            if(a%10+b%10+carry>=10){
+                cnt++;
+                carry=1;
+            }
+            else carry=0;
+            a/=10;
+            b/=10;
+        }
+        if(cnt==0){
+            cout<<"No carry operation."<<endl;
+        }
+        else if(cnt==1){
+            cout<<"1 carry operation."<<endl;
+        }
         else {
-            int cnt=0;
-            int carry=0;
-            while(a>0||b>0){
-                int sum=a%10+b%10+carry;
-                if(sum>=10){
-                   cnt++;
-                   carry=1;
-                }
-                else{
-                    carry=0;
-                }
-                a/=10;
-                b/=10;
-            }
-            if(cnt==0){
-                cout<<"No carry operation."<<endl;
-            }
-            else if(cnt==1){
-                cout<<"1 carry operation."<<endl;
-            }
-            else {
-                cout<<cnt<<" carry operations."<<endl;
-            }
+            cout<<cnt<<" carry operations."<<endl;
         }
     }
 }
